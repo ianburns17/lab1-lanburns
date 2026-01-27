9d469cb4a3a67f63a5a4e2ed1669f30e54d9b88e
ianburns17 <2023158285@ub.edu.bz>
2026-01-27 01:20:55 +0000

    Add contact page handler

--- web/cmd/main.go
+++ web/cmd/main.go
@@ -11,13 +11,16 @@ func home(w http.ResponseWriter, r *http.Request) {
 func about(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("About me- My name is Ian Burns and i Love cyber security\n"))
 }
-
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Contact me at:\n email: ianburns344@gmail.com \n phone number: 6259908 "))
+}
 func main() {
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/about", about)
+	mux.HandleFunc("/contact", contact)
 
 	log.Print("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
