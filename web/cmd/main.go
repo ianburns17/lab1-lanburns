e6138024caef05ffe449163d79c10d0807086bcd
ianburns17 <2023158285@ub.edu.bz>
2026-01-27 01:19:02 +0000

    Add about page route and handler

--- web/cmd/main.go
+++ web/cmd/main.go
@@ -8,12 +8,16 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from newsletter\n"))
 }
+func about(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("About me- My name is Ian Burns and i Love cyber security\n"))
+}
 
 func main() {
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/about", about)
 
 	log.Print("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
